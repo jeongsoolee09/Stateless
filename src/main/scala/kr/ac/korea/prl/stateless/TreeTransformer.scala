b60689dc43c9b0c1b392329506558dff18da682f
jeongsoolee09 <volcano0909@gmail.com>
2021-06-22 17:20:28 +0900

    update TreeTransformer to work on CustomTree, not scala.meta.Tree

--- src/main/scala/kr/ac/korea/prl/stateless/TreeTransformer.scala
+++ src/main/scala/kr/ac/korea/prl/stateless/TreeTransformer.scala
@@ -4,8 +4,7 @@ import scala.annotation.tailrec
 import scala.meta._
 
 import kr.ac.korea.prl.stateless.TreeTraverser._
-
-object TODO extends Exception
+import kr.ac.korea.prl.stateless.CustomTree._
 
 class TreeTransformer {
 
@@ -15,15 +14,15 @@ class TreeTransformer {
     * @param tree
     * @return
     */
-  def isToplevel(varTup: (Type.Name, Term.Name, Term.Name,
-                          Option[Type], Option[Term]),
-                 tree: Tree) = {
+  def isToplevel(varTup: (TypeName, TermName, TermName,
+                          Option[CustomType], Option[CustomTerm]),
+                 tree: CustomTree) = {
     val innerClassInfo = TreeTraverser.innerClassChainCollector(tree)
-    val toplevelClasses = innerClassInfo.foldLeft(List[Type.Name]())((acc, tup) =>
-      if (tup._1 == Type.Name("ph")) tup._1::acc else acc
+    val toplevelClasses = innerClassInfo.foldLeft(List[TypeName]())((acc, tup) =>
+      if (tup._1 equals TypeName("ph")) tup._1::acc else acc
     )
     toplevelClasses.foldLeft(false)((acc, classType) =>
-      (classType == varTup._1) || acc)
+      (classType equals varTup._1) || acc)
   }
 
 
@@ -33,9 +32,9 @@ class TreeTransformer {
     * @param tree
     * @return tuples describing all toplevel vars
     */
-  def toplevelVarSpotter(tree: Tree): List[(Type.Name, Term.Name, Term.Name,
-                                             Option[Type], Option[Term])] =
-    TreeTraverser.varCollector(tree).filter((isToplevel(_, tree)))
+  def toplevelVarSpotter(tree: CustomTree): List[(TypeName, TermName, TermName,
+                                             Option[CustomType], Option[CustomTerm])] =
+    TreeTraverser.varCollector(tree).filter(isToplevel(_, tree))
 
 
   /**
@@ -44,20 +43,20 @@ class TreeTransformer {
     * @param tree
     * @return the same tree with toplevel vars removed.
     */
-  def toplevelVarRemover(tree: Tree): Tree = {
+  def toplevelVarRemover(tree: CustomTree): CustomTree = {
     val toplevelVars = toplevelVarSpotter(tree)
     tree match {
-      case Defn.Class(mods, name, tparams, ctor, Template(early, inits, self, stats)) => {
+      case DefClass(mods, name, tparams, ctor, CustomTemplate(early, inits, self, stats)) => {
                         val filtered = stats.filter(toplevelVars.contains(_))
-                        Defn.Class(mods, name, tparams, ctor, Template(early, inits, self, filtered))
+                        DefClass(mods, name, tparams, ctor, CustomTemplate(early, inits, self, filtered))
       }
-      case Source(stats) =>
-        Source(stats.filter(TreeTraverser.scopeGreaterThanClass)
-                 .map(toplevelVarRemover(_).asInstanceOf[Stat]))
+      case CustomSource(stats) =>
+        CustomSource(stats.filter(TreeTraverser.scopeGreaterThanClass)
+                 .map(toplevelVarRemover(_).asInstanceOf[CustomStat]))
 
-      case Pkg(ref, stats) =>
-        Pkg(ref, stats.filter(TreeTraverser.scopeGreaterThanClass)
-              .map(toplevelVarRemover(_).asInstanceOf[Stat]))
+      case CustomPkg(ref, stats) =>
+        CustomPkg(ref, stats.filter(TreeTraverser.scopeGreaterThanClass)
+              .map(toplevelVarRemover(_).asInstanceOf[CustomStat]))
     }
   }
 
