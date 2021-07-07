31bbee03df2ad651681f27d7e2e50c94f15165e9
jeongsoolee09 <volcano0909@gmail.com>
2021-07-07 16:38:11 +0900

    update summarizeCustomTree

--- src/main/scala/kr/ac/korea/prl/stateless/SummarizedTreeGraph.scala
+++ src/main/scala/kr/ac/korea/prl/stateless/SummarizedTreeGraph.scala
@@ -22,151 +22,78 @@ object SummarizedTreeGraph {
   val summarizeCustomTree: CustomTree => String =
     (ctree: CustomTree) => ctree match {
       case current: CustomInit => current.productPrefix
-
       case current: CustomSelf => current.productPrefix
-
-      case current: TypeParam => current.productPrefix
-
-      case current: TypeName => current.productPrefix
-
+      case current: TypeParam => current.toString  // atom
+      case current: TypeName => current.toString  // atom
       case current: TypeApply => current.productPrefix
-
       case current: TypeBounds => current.productPrefix
-
       case current: Generator => current.productPrefix
-
       case current: CaseGenerator => current.productPrefix
-
       case current: Val => current.productPrefix
-
       case current: Guard => current.productPrefix
-
-      case current: TermName => current.productPrefix
-
+      case current: TermName => current.toString  // atom
       case current: TermParam => current.productPrefix
-
       case current: TermLambda => current.productPrefix
-
       case current: TermSelect => current.productPrefix
-
       case current: TermInterpolate => current.productPrefix
-
       case current: TermApply => current.productPrefix
-
       case current: TermApplyUsing => current.productPrefix
-
       case current: TermApplyType => current.productPrefix
-
       case current: TermApplyInfix => current.productPrefix
-
       case current: TermApplyUnary => current.productPrefix
-
       case current: TermAssign => current.productPrefix
-
       case current: TermReturn => current.productPrefix
-
       case current: TermNew => current.productPrefix
-
       case current: TermBlock => current.productPrefix
-
       case current: TermIf => current.productPrefix
-
       case current: TermTry => current.productPrefix
-
       case current: TermWhile => current.productPrefix
-
       case current: TermFor => current.productPrefix
-
       case current: TermThrow => current.productPrefix
-
       case current: TermSuper => current.productPrefix
-
       case current: TermThis => current.productPrefix
-
-      case current: LitNull => "LitNull"
-
-      case current: LitInt => current.productPrefix
-
-      case current: LitDouble => current.productPrefix
-
-      case current: LitFloat => current.productPrefix
-
-      case current: LitByte => current.productPrefix
-
-      case current: LitShort => current.productPrefix
-
-      case current: LitChar => current.productPrefix
-
-      case current: LitLong => current.productPrefix
-
-      case current: LitBoolean => current.productPrefix
-
-      case current: LitUnit => current.productPrefix
-
-      case current: LitString => current.productPrefix
-
-      case current: LitSymbol => current.productPrefix
-
+      case current: LitNull => "LitNull"           // atom
+      case current: LitInt => current.toString  // atom
+      case current: LitDouble => current.toString  // atom
+      case current: LitFloat => current.toString  // atom
+      case current: LitByte => current.toString  // atom
+      case current: LitShort => current.toString  // atom
+      case current: LitChar => current.toString  // atom
+      case current: LitLong => current.toString  // atom
+      case current: LitBoolean => current.toString  // atom
+      case current: LitUnit => current.toString  // atom
+      case current: LitString => current.toString  // atom
+      case current: LitSymbol => current.toString  // atom
       case current: Private => current.productPrefix
-
       case current: Protected => current.productPrefix
-
       case current: Implicit => current.productPrefix
-
       case current: Abstract => current.productPrefix
-
       case current: Override => current.productPrefix
-
       case current: Super => current.productPrefix
-
       case current: Final => current.productPrefix
-
       case current: DefVal => current.productPrefix
-
       case current: DefVar => current.productPrefix
-
       case current: DefDef => current.productPrefix
-
       case current: DefEnum => current.productPrefix
-
       case current: DefClass => current.productPrefix
-
       case current: DefObject => current.productPrefix
-
       case current: DefTrait => current.productPrefix
-
       case current: CustomTemplate => current.productPrefix
-
       case current: CustomSource => current.productPrefix
-
       case current: CustomPkg => current.productPrefix
-
       case current: CustomImport => current.productPrefix
-
       case current: PatVar => current.productPrefix
-
-      case current: CustomName => current.productPrefix
-
+      case current: CustomName => current.toString  // atom
       case current: PrimaryCtor => current.productPrefix
-
       case current: SecondaryCtor => current.productPrefix
-
       case current: CustomCase => current.productPrefix
-
       case current: CustomImporter => current.productPrefix
-
       case current: DeclDef => current.productPrefix
-
       case current: DeclVar => current.productPrefix
-
       case current: DeclVal => current.productPrefix
-
       case current: ImporteeGiven => current.productPrefix
-
       case current: ImporteeGivenAll => current.productPrefix
-
       case current: ImporteeName => current.productPrefix
-
       case current: ImporteeWildcard => current.productPrefix
     }
 
